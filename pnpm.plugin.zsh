8b354b498230d57215182881f63fed41b4235309
Kuroi Kyu <kuroikyu@users.noreply.github.com>
2022-06-09 20:33:05 +0200

    feat: added aliases
    Signed-off-by: Kuroi Kyu <kuroikyu@users.noreply.github.com>

---
+++ pnpm.plugin.zsh
@@ -0,0 +1,25 @@
+alias p="pnpm"
+
+alias pa="pnpm add"
+alias pad="pnpm add --save-dev"
+alias pag="pnpm add --global"
+alias prm="pnpm remove"
+
+alias pu="pnpm update"
+alias pui='pnpm update --interactive'
+alias puil='pnpm update --interactive --latest'
+
+alias pi="pnpm install"
+
+alias pd="pnpm dev"
+alias pb="pnpm build"
+alias ps="pnpm serve"
+alias pst="pnpm start"
+alias pt="pnpm test"
+
+alias pls="pnpm list"
+alias pout="pnpm outdated"
+
+alias px="pnpm dlx"
+alias pcr="pnpm create"
+alias penv="pnpm env"
